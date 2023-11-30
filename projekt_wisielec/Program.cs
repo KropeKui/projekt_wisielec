4cd50673b3411a3eb82cd799b82239820db8ea94
wikto <wikto@KISIEL-LAPTOP>
2023-11-30 14:32:28 +0100

    Dodaj pliki projektów.

---
+++ projekt_wisielec/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace projekt_wisielec
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w grze WISIELEC");
+            Console.WriteLine("Podaj hasło do gry:");
+            string haslo = Console.ReadLine();
+            double haslo1 = haslo.Length;
+            Console.WriteLine(haslo);
+            Console.WriteLine(haslo1);
+
+            String[] litery = Array.Empty<string>();
+
+            for (int i = 0; i < haslo1; i++)
+            {
+                Console.Write("_ ");
+
+                
+                //litery = litery.Concat(new string[] { haslo1[i] }).ToArray();
+
+            }
+
+
+
+
+
+        }
+    }
+}
